b0692c5e523b1c0d33d9bb092ad255a4256d40f9
Rohan Paul <rohanpaul2@gmail.com>
2020-02-27 01:02:50 +0530

    updating notes on Promise.all()

---
+++ With-React-Hooks/build/precache-manifest.7c67428668d92164d35343165e596ee1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "315b225d08376c33311099ae23112268",
+    "url": "/static/media/bg_purple.315b225d.png"
+  },
+  {
+    "revision": "a31cc126b208b8efa51b",
+    "url": "/static/css/main.6d1e3c34.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dcc3e306908a50719b1100479cf8eaae",
+    "url": "/static/media/astronaut.dcc3e306.svg"
+  },
+  {
+    "revision": "89ec606afdff85498221",
+    "url": "/static/js/2.87f429e6.chunk.js"
+  },
+  {
+    "revision": "53fe152c4b08d684d8a8ed126f3173b2",
+    "url": "/static/media/car.53fe152c.svg"
+  },
+  {
+    "revision": "2ef7ada74a965bcb65a38c81f47158be",
+    "url": "/static/media/images.2ef7ada7.jpg"
+  },
+  {
+    "revision": "de325276d6e02b1d184f3719fc91ef9b",
+    "url": "/static/media/overlay_stars.de325276.svg"
+  },
+  {
+    "revision": "a31cc126b208b8efa51b",
+    "url": "/static/js/main.75512cdb.chunk.js"
+  },
+  {
+    "revision": "fd07dc86af75daade91453bc49863be2",
+    "url": "/static/media/logo.fd07dc86.svg"
+  },
+  {
+    "revision": "5b71688c66ca8e1e3fd478a0afb7a756",
+    "url": "/static/media/404.5b71688c.svg"
+  },
+  {
+    "revision": "be464a54fd815eef1f3b18f7588321f3",
+    "url": "/static/media/rocket.be464a54.svg"
+  },
+  {
+    "revision": "dbfa284b6b78656188dc65ea25f7484c",
+    "url": "/static/media/earth.dbfa284b.svg"
+  },
+  {
+    "revision": "551a0cd56eda4a7fc38f20e8b86bb458",
+    "url": "/static/media/moon.551a0cd5.svg"
+  },
+  {
+    "revision": "89ec606afdff85498221",
+    "url": "/static/css/2.762f1001.chunk.css"
+  },
+  {
+    "revision": "e3aee11c92b2113aca4aab12a54ce793",
+    "url": "/index.html"
+  }
+];
--- With-React-Hooks/src/Dashboard.js
+++ With-React-Hooks/src/Dashboard.js
@@ -1,73 +1,73 @@
-import React, { Component, useState, useEffect, useRef } from "react";
-import axios from "axios";
-import MUIDataTable from "mui-datatables";
-import "./Dashboard.css";
-import NewItemAddedConfirmSnackbar from "./NewItemAddedConfirmSnackbar";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-const isEqual = require("lodash.isequal");
-const differenceWith = require("lodash.differencewith");
-const omit = require("lodash.omit");
+import React, { Component, useState, useEffect, useRef } from "react"
+import axios from "axios"
+import MUIDataTable from "mui-datatables"
+import "./Dashboard.css"
+import NewItemAddedConfirmSnackbar from "./NewItemAddedConfirmSnackbar"
+import TextField from "@material-ui/core/TextField"
+import Button from "@material-ui/core/Button"
+const isEqual = require("lodash.isequal")
+const differenceWith = require("lodash.differencewith")
+const omit = require("lodash.omit")
 
 const getEachStoryGivenId = (id, index) => {
   return new Promise((resolve, reject) => {
     axios
       .get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
       .then(res => {
-        let story = res.data;
-        let result = omit(story, ["descendants", "time", "id", "type"]);
+        let story = res.data
+        let result = omit(story, ["descendants", "time", "id", "type"])
         if (
           result &&
           Object.entries(result).length !== 0 &&
           result.constructor === Object
         ) {
-          resolve(result);
+          resolve(result)
         } else {
-          reject(new Error("No data received"));
+          reject(new Error("No data received"))
         }
-      });
-  });
-};
+      })
+  })
+}
 
 const Dashboard = () => {
-  const [prevStoriesIds, setPrevStoriesIds] = useState([]);
-  const [fetchedData, setFetchedData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [tableState, setTableState] = useState({});
+  const [prevStoriesIds, setPrevStoriesIds] = useState([])
+  const [fetchedData, setFetchedData] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [tableState, setTableState] = useState({})
   const [
     openNewItemAddedConfirmSnackbar,
-    setOpenNewItemAddedConfirmSnackbar
-  ] = useState(false);
-  const [noOfNewStoryAfterPolling, setNoOfNewStoryAfterPolling] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
+    setOpenNewItemAddedConfirmSnackbar,
+  ] = useState(false)
+  const [noOfNewStoryAfterPolling, setNoOfNewStoryAfterPolling] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10)
 
   const onChangeRowsPerPage = rowsPerPage => {
-    setRowsPerPage(rowsPerPage);
-  };
+    setRowsPerPage(rowsPerPage)
+  }
 
   const closeNewItemConfirmSnackbar = () => {
-    setOpenNewItemAddedConfirmSnackbar(false);
+    setOpenNewItemAddedConfirmSnackbar(false)
     axios
       .get("https://hacker-news.firebaseio.com/v0/newstories.json")
       .then(storyIds => {
-        setPrevStoriesIds(storyIds.data.slice(0, 2));
-        getAllNewStory(storyIds);
-      });
-  };
+        setPrevStoriesIds(storyIds.data.slice(0, 2))
+        getAllNewStory(storyIds)
+      })
+  }
 
   const getAllNewStory = storyIds => {
-    setIsLoading(true);
-    let topStories = storyIds.data.slice(0, 2).map(getEachStoryGivenId);
-    let results = Promise.all(topStories);
+    setIsLoading(true)
+    let topStories = storyIds.data.slice(0, 2).map(getEachStoryGivenId)
+    let results = Promise.all(topStories)
     results
       .then(res => {
-        setFetchedData(res);
-        setIsLoading(false);
+        setFetchedData(res)
+        setIsLoading(false)
       })
       .catch(err => {
-        console.log(err);
-      });
-  };
+        console.log(err)
+      })
+  }
 
   /*
 The API endpoint - /newstories: Provides a list of up to 500 story IDs, the newest first.
@@ -81,20 +81,20 @@ Set the state array with all of the fetched story objects */
   const usePrevious = value => {
     // The ref object is a generic container whose current property is mutable
     // And ref can hold any value, similar to an instance property on a class
-    const ref = useRef();
+    const ref = useRef()
 
     // Store current value in ref
     useEffect(() => {
-      ref.current = value;
-    });
+      ref.current = value
+    })
 
     // Return previous value (happens before update in useEffect above)
-    return ref;
-  };
+    return ref
+  }
 
-  const fromPrevStoriesIds = usePrevious(prevStoriesIds);
+  const fromPrevStoriesIds = usePrevious(prevStoriesIds)
 
-  const intervalRef = useRef();
+  const intervalRef = useRef()
 
   useEffect(() => {
     const fetchData = () => {
@@ -103,11 +103,11 @@ Set the state array with all of the fetched story objects */
         .then(storyIds => {
           //   console.log("STORY IDs FETCHED ", storyIds.data.slice(0, 2));
 
-          setPrevStoriesIds(storyIds.data.slice(0, 2));
-          getAllNewStory(storyIds);
-        });
-    };
-    fetchData();
+          setPrevStoriesIds(storyIds.data.slice(0, 2))
+          getAllNewStory(storyIds)
+        })
+    }
+    fetchData()
 
     const doPolling = () => {
       const timer = setInterval(() => {
@@ -120,42 +120,42 @@ Set the state array with all of the fetched story objects */
               fromPrevStoriesIds !== undefined &&
               !isEqual(
                 fromPrevStoriesIds.current.sort(),
-                storyIds.data.slice(0, 2).sort()
+                storyIds.data.slice(0, 2).sort(),
               )
             ) {
-              setPrevStoriesIds(storyIds.data.slice(0, 2));
+              setPrevStoriesIds(storyIds.data.slice(0, 2))
               setNoOfNewStoryAfterPolling(
                 differenceWith(
                   fromPrevStoriesIds.current.sort(),
                   storyIds.data.slice(0, 2).sort(),
-                  isEqual
-                ).length
-              );
-              getAllNewStory(storyIds);
-              setOpenNewItemAddedConfirmSnackbar(true);
+                  isEqual,
+                ).length,
+              )
+              getAllNewStory(storyIds)
+              setOpenNewItemAddedConfirmSnackbar(true)
             }
-          });
-      }, 10000);
+          })
+      }, 10000)
 
-      intervalRef.current = timer;
-    };
+      intervalRef.current = timer
+    }
 
-    doPolling();
+    doPolling()
 
     return () => {
-      console.log("cleaning up");
-      clearInterval(intervalRef.current);
-    };
-  }, [rowsPerPage, noOfNewStoryAfterPolling]);
+      console.log("cleaning up")
+      clearInterval(intervalRef.current)
+    }
+  }, [rowsPerPage, noOfNewStoryAfterPolling])
 
   // Conditionally set the value of 'renderedStoriesOnPage' to show to the page view by executing the below IIFE -  for formatting the dates from UTC to human-readeable format - getDataToRender()
-  let renderedStoriesOnPage = [];
+  let renderedStoriesOnPage = []
   const getDataToRender = (() => {
     renderedStoriesOnPage = fetchedData.map(i => {
-      return Object.values(i);
-    });
-    return renderedStoriesOnPage;
-  })();
+      return Object.values(i)
+    })
+    return renderedStoriesOnPage
+  })()
 
   // For each of the column its sortDirection property will come from the app state (if the user has already changed the table and sorted a column) ELSE its set to null.
   // So this is where the requirement "Persistent Sorting" should have been implemented - "On every poll the current sorting option should remain unchanged e.g. if user has sorted the list of stories based on score then that order should remain unchanged"  - But due to an open issue on the package [Table sort is lost if data changed on a re-render](https://github.com/gregnb/mui-datatables/issues/305) - sorting is not getting persisted.
@@ -164,21 +164,21 @@ Set the state array with all of the fetched story objects */
       name: "Author",
       sortDirection: tableState
         ? tableState.columns && tableState.columns[0].sortDirection
-        : null
+        : null,
     },
 
     {
       name: "score",
       sortDirection: tableState
         ? tableState.columns && tableState.columns[1].sortDirection
-        : null
+        : null,
     },
 
     {
       name: "title",
       sortDirection: tableState
         ? tableState.columns && tableState.columns[2].sortDirection
-        : null
+        : null,
     },
 
     {
@@ -191,11 +191,11 @@ Set the state array with all of the fetched story objects */
             <a target="_blank" href={value}>
               {value}
             </a>
-          );
-        }
-      }
-    }
-  ];
+          )
+        },
+      },
+    },
+  ]
 
   const options = {
     filter: true,
@@ -208,9 +208,9 @@ Set the state array with all of the fetched story objects */
     activeColumn: tableState ? tableState.activeColumn : 0,
     onTableChange: (action, tableState) => {
       // console.log("taBLE STATE IS ", JSON.stringify(tableState));
-      setTableState(tableState);
-    }
-  };
+      setTableState(tableState)
+    },
+  }
 
   return (
     <React.Fragment>
@@ -219,7 +219,7 @@ Set the state array with all of the fetched story objects */
           marginLeft: "15px",
           marginTop: "80px",
           display: "flex",
-          flexDirection: "row"
+          flexDirection: "row",
         }}
       >
         <h4 style={{ width: "400px", paddingRight: "15px" }}>
@@ -251,10 +251,10 @@ Set the state array with all of the fetched story objects */
         />
       </div>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
 
 /* A> Notes on Object.assign -
 To Merge multiple sources
@@ -270,12 +270,16 @@ C> All the new story {fetchedData: res}
 
 A note on usage of Promise.all(…) above for chaining the update of the component's state to the resulting Promise.
 
+It is typically used after having started multiple asynchronous tasks to run concurrently and having created promises for their results, so that one can wait for all the tasks being finished.
+
 The Promise.all() takes an array (or any iterable) of promises and returns a single Promise that resolves when all of the promises passed as an iterable have resolved or when the iterable contains no promises. If any of the passed-in promises reject, Promise.all asynchronously rejects with the value of the promise that rejected, whether or not the other promises have resolved. 
 
-Promise.all() method is useful when we have multiple events and has to wait for more than one promise to complete which is the case here. 
-Also the returned values from Promise.all() will be in the order of the promises in the iterable regardless of the order of resolution of promises. This gives us a clue that the promise resolution follows a serial order of execution.
+Promise.all() method is useful when we have multiple events and have to wait for more than one promise to complete which is the case here. 
 
-So in this way, getAllNewStory() function will always wait until the data for all of the stories has been fetched, before updating the app's state in one single call. 
+
+Also the returned values from Promise.all() will be in the order of the promises in the iterable regardless of the order of resolution of promises. This gives us a clue that the promise resolution follows a serial order of execution. That is, Promise.all preserves the order of the promises is maintained. The first promise in the array will get resolved to the first element of the output array, the second promise will be a second element in the output array and so on.
+
+So in this way, getAllNewStory() function will always wait until the data for all of the stories have been fetched, before updating the app's state in one single call. 
 Here's how it will work - The getEachStoryGivenId() function returns a Promise, which when resolved gives me the story of the given ID (passed in as the argument to getEachStoryGivenId )
 
 And then getAllNewStory() function will invoke the above as below 
@@ -283,5 +287,7 @@ And then getAllNewStory() function will invoke the above as below 
 topStories = storyIds.data.slice(0, 2).map(getEachStoryGivenId);
 let results = Promise.all(topStories);
 
-So the variable result will wait until all the Promises have been resolved (or upto the first reject). And the result will have the PromiseValue
+So the variable result will wait until all the Promises have been resolved (or upto the first reject). And the result will have the PromiseValue.abs
+
+Then the then() method and the catch() method can be called on this promise which is returned from Promise.all. The then() method gets an array of all the resolved values, [5, 1, 'foo'] as an example. The catch() method gets the value of the first rejected Promise,
  */
